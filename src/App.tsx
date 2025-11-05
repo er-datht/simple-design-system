c936b5cde1223d0601a52310a274c2b25dc35197
Dat Ha Thuc <datht@tech.est-rouge.com>
2025-11-05 16:42:25 +0700

    Implement get all icons from figma and setup icon components in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  IconActivity,
+  IconAirplay,
+  IconAlertCircle,
+  IconAlertTriangle,
+  IconAlignLeft,
+} from "./ui/icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex gap-1.5 justify-between items-center">
+      <IconActivity size="40" />
+      <IconAirplay size="40" />
+      <IconAlertCircle size="40" />
+      <IconAlertTriangle size="40" />
+      <IconAlignLeft size="40" />
+    </div>
+  );
 }
 
-export default App
+export default App;
